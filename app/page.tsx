df16cb5e115c2dfeac3f167f9c08de16d6a44760
Dillon Shearer <dillshearer@outlook.com>
2025-04-03 19:44:59 -0400

    update site

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,60 @@
 import { BlogPosts } from 'app/components/posts'
 
+const projects = [
+  {
+    name: 'State-by-State Analysis of Chronic Condition Drug Utilization and Costs',
+    link: 'https://github.com/dillon-shearer/portfolio/tree/main/ChronicConditions_PrescriptionDrugs_Project'
+  },
+  {
+    name: '2022 Census SAIPE Poverty Data Analysis and Visualization',
+    link: 'https://github.com/dillon-shearer/portfolio/tree/main/census_Poverty'
+  },
+  {
+    name: 'Reddit TikTok Script',
+    link: 'https://github.com/dillon-shearer/portfolio/tree/main/reddit-tiktok-script'
+  }
+]
+
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Dillon Shearer | Data Science & Analytics
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
+
+      {/* About Me Section */}
+      <div className="mb-8">
+        <h2 className="text-xl font-medium">About Me:</h2>
+        <p className="mb-4">
+          {`I am a dedicated data science and analytics professional focused on healthcare. I leverage advanced statistical techniques, machine learning, and data visualization to extract meaningful insights from complex health data. My goal is to transform raw data into strategic decisions that improve patient outcomes and optimize healthcare operations.`}
+        </p>
+      </div>
+
+      {/* Projects Section */}
       <div className="my-8">
-        <BlogPosts />
+        <h2 className="text-xl font-medium">Projects:</h2>
+        <ul className="list-disc ml-5 mt-4">
+          {projects.map((project) => (
+            <li key={project.name}>
+              <a 
+                href={project.link} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                {project.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Contact Section */}
+      <div className="my-8">
+        <h2 className="text-xl font-medium">Contact:</h2>
+        <p>
+          Email: <b>dillon@datawithdillon.com</b>
+        </p>
       </div>
     </section>
   )
