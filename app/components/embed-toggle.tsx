ef1a607e5f976087220e1a568da880e4c717a519
Dillon Shearer <dillshearer@outlook.com>
2026-02-02 00:07:58 -0500

    patch header/footer bug for koreader remote

--- app/components/embed-toggle.tsx
+++ app/components/embed-toggle.tsx
@@ -3,18 +3,25 @@
 import { useEffect } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 
+const HIDE_CHROME_ROUTES = ['/koreader-remote']
+
 export default function EmbedToggle() {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const isEmbed = searchParams.get('embed') === '1'
+  const isChromeHiddenRoute = HIDE_CHROME_ROUTES.some((route) => {
+    if (!pathname) return false
+    return pathname === route || pathname.startsWith(`${route}/`)
+  })
+  const shouldHideChrome = isEmbed || isChromeHiddenRoute
 
   useEffect(() => {
-    if (isEmbed) {
+    if (shouldHideChrome) {
       document.body.dataset.embedPreview = 'true'
     } else {
       delete document.body.dataset.embedPreview
     }
-  }, [isEmbed, pathname])
+  }, [shouldHideChrome])
 
   return null
 }
